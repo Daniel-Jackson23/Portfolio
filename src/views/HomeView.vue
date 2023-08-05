cd9cecca3b1d66594daf8f9f379baf4329c4d235
Daniel Jackson <80455667+Daniel-Jackson23@users.noreply.github.com>
2023-08-05 18:06:25 +0100

    fixing mobile view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,7 +4,10 @@ import HomeBanner from '../components/HomeBanner.vue';
 
 </script>
 
-<template >
+<template class="container
+px-5
+py-3
+mx-auto flex flex-row sm:max-lg:flex-col" >
   <main>
     <HomeBanner />
   </main>
